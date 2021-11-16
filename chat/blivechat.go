1eb9a6690f66ad3dfba204bbeb44d183d86a2047
Aynakeya <aynakeya@aynakeya.com>
2021-11-15 22:11:41 -0800

    update blivechat.go

--- chat/blivechat.go
+++ chat/blivechat.go
@@ -10,30 +10,41 @@ import (
 	"github.com/aynakeya/blivedm"
 	"github.com/aynakeya/gocui"
 	"github.com/spf13/cast"
+	"gopkg.in/ini.v1"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
-func main() {
-	args := os.Args[1:]
+type ConfigFile struct {
+	Uid         int
+	SessionData string
+	BilibiliJCT string
 
-	if len(args) < 1 {
-		defer func() {
-			fmt.Print("Press entet to quit")
-			var a string
-			if _, err := fmt.Scanln(&a); err != nil {
-				return
-			}
-		}()
-		fmt.Println("Usage blivechat <room_id> Optional[<uid> <sessdata> <bilijct>]")
+	VisualColorMode bool
+}
+
+func saveToConfig(cfg ConfigFile) {
+	cfgFile := ini.Empty()
+	cfgFile.Section("blivechat").Key("Uid").SetValue(cast.ToString(cfg.Uid))
+	cfgFile.Section("blivechat").Key("VisualColorMode").SetValue(cast.ToString(cfg.VisualColorMode))
+	cfgFile.Section("blivechat").Key("SessionData").SetValue(cast.ToString(cfg.SessionData))
+	cfgFile.Section("blivechat").Key("BilibiliJCT").SetValue(cast.ToString(cfg.BilibiliJCT))
+	err := cfgFile.SaveTo("blivechat.ini")
+	if err != nil {
 		return
 	}
-	roomId := cast.ToInt(args[0])
-	if roomId == 0 {
-		fmt.Println("Room id is not proper")
-		return
+}
+
+func getOrDefault(input string, defaultValue string) string {
+	if input == "" {
+		return defaultValue
 	}
+	return input
+}
+
+func startFromArgs(roomId int, args []string) *gocui.Gui {
 	uid := 0
 	sessdata := ""
 	bilijct := ""
@@ -43,10 +54,6 @@ func main() {
 		bilijct = cast.ToString(args[3])
 	}
 	g := blivechat.CreateGUI()
-	defer func() {
-		g.Cursor = false
-		g.Close()
-	}()
 	cl := blivedm.BLiveWsClient{ShortId: roomId,
 		Account: blivedm.DanmuAccount{
 			UID:         uid,
@@ -55,6 +62,100 @@ func main() {
 		},
 		HearbeatInterval: 25 * time.Second}
 	blivechat.SetupDanmuClient(g, &cl)
+	saveToConfig(ConfigFile{
+		Uid:             uid,
+		SessionData:     sessdata,
+		BilibiliJCT:     bilijct,
+		VisualColorMode: false,
+	})
+	return g
+}
+
+func startFromConfig(roomId int, filename string) *gocui.Gui {
+	cfg := ConfigFile{
+		Uid:             0,
+		SessionData:     "",
+		BilibiliJCT:     "",
+		VisualColorMode: false,
+	}
+	cfgFile, err := ini.Load(filename)
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		return nil
+	}
+
+	cfg.Uid = cast.ToInt(getOrDefault(cfgFile.Section("blivechat").
+		Key("Uid").Value(), "0"))
+	cfg.VisualColorMode = cast.ToBool(
+		getOrDefault(cfgFile.Section("blivechat").
+			Key("VisualColorMode").Value(), "false"))
+	cfg.SessionData = getOrDefault(cfgFile.Section("blivechat").
+		Key("SessionData").Value(), "")
+	cfg.BilibiliJCT = getOrDefault(cfgFile.Section("blivechat").
+		Key("BilibiliJCT").Value(), "")
+	g := blivechat.CreateGUI()
+	cl := blivedm.BLiveWsClient{ShortId: roomId,
+		Account: blivedm.DanmuAccount{
+			UID:         cfg.Uid,
+			SessionData: cfg.SessionData,
+			BilibiliJCT: cfg.BilibiliJCT,
+		},
+		HearbeatInterval: 25 * time.Second}
+	blivechat.SetupDanmuClient(g, &cl)
+	return g
+}
+
+func main() {
+	var g *gocui.Gui
+	args := os.Args[1:]
+	roomId := 0
+
+	if len(args) < 1 {
+		defer func() {
+			fmt.Print("Press entet to quit")
+			var a string
+			if _, err := fmt.Scanln(&a); err != nil {
+				return
+			}
+		}()
+		fmt.Println("Usage blivechat <room_id> Optional[<uid> <sessdata> <bilijct>]\n" +
+			"	blivechat <room_id> --c=<config file name>")
+		fmt.Println("Please enter room id > ")
+		_, err := fmt.Scanln(&roomId)
+		if err != nil {
+			return
+		}
+	} else {
+		roomId = cast.ToInt(args[0])
+	}
+	if roomId == 0 {
+		fmt.Println("Room id is not proper")
+		return
+	}
+	useConfig := false
+	configName := "blivechat.ini"
+	g = startFromConfig(roomId, configName)
+	if g == nil {
+		for _, arg := range args {
+			if strings.HasPrefix(arg, "--c") {
+				useConfig = true
+				configName = strings.ReplaceAll(arg, "--c=", "")
+				break
+			}
+		}
+		if useConfig {
+			g = startFromConfig(roomId, configName)
+		} else {
+			g = startFromArgs(roomId, args)
+		}
+	}
+	if g == nil {
+		return
+	}
+	defer func() {
+		g.Cursor = false
+		g.Close()
+	}()
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aynakeya/gocui v1.0.2
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/spf13/cast v1.3.1
+	gopkg.in/ini.v1 v1.64.0 // indirect
 )
 
 //replace (
